b6b1978ca0d38e06539474fb827378e42c53d533
jonataslaet <jonatas@infoway-pi.com.br>
2020-07-01 19:27:20 -0300

    Criações e ajustes Criação do service da calculadora Alteração do html do componente da calculadora Instalação e inclusão do bootstrap

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -1,3 +1,4 @@
+import { CalculadoraService } from './../services/calculadora.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit(): void {
   }
