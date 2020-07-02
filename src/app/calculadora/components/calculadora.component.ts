30089ef3b4eb4bcee6995171719968c3b6a96680
jonataslaet <jonatas@infoway-pi.com.br>
2020-07-02 18:09:49 -0300

    Implementada e colocada a logica das operaçoes

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -7,10 +7,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
+  private numero1: string;
+  private numero2: string;
+  private resultado: number;
+  private operacao: string;
 
   constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit(): void {
+    this.limpar();
   }
 
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  adicionarNumero(numero: string): void {
+    if (this.operacao === null){
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    } else {
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  }
+
+  concatenarNumero(numero: string, numConcat: string): string {
+    if (numero === '0' || numero === null){
+      numero = '';
+    }
+
+    if (numConcat === '.' && numero === ''){
+      return '0.';
+    }
+
+    if (numConcat === '.' && numero.indexOf('.') > -1){
+      return numero;
+    }
+
+    return numero + numConcat;
+  }
+
+  definirOperacao(operacao: string): void {
+    if (this.operacao === null){
+      this.operacao = operacao;
+      return;
+    }
+
+    if (this.numero2 !== null){
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao
+      );
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = null;
+      this.resultado = null;
+    }
+  }
+
+  calcular(): void{
+    if (this.numero2 === null){
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.numero1),
+      parseFloat(this.numero2),
+      this.operacao
+    );
+  }
+
+  get display(): string {
+    if (this.resultado !== null){
+      return this.resultado.toString();
+    }
+    if (this.numero2 !== null) {
+      return this.numero2;
+    }
+    return this.numero1;
+  }
 }
